1e8b605dd0301af5bfd1198dd61f10e6d88ee1c2
Suraj Rathi <suraj.rathi00@gmail.com>
2021-01-12 00:22:24 +0530

    started adding rti_connext template

--- iac_planner/main.py
+++ iac_planner/main.py
@@ -3,6 +3,7 @@
 Visualizes the CTE of some simple paths using rviz2
 """
 import sys
+from math import sqrt
 from typing import Optional, Iterable, Callable
 import logging
 
@@ -10,9 +11,14 @@ import numpy as np
 # from std_msgs.msg import ColorRGBA
 
 # from iac_planner.generate_markers import visualize
+from rticonnextdds_connector import Input
+
 from iac_planner.generate_paths import generate_paths
 from iac_planner.helpers import Env, state_t
 from iac_planner.score_paths import score_paths
+
+import rticonnextdds_connector as rti
+
 
 def main(args: Optional[Iterable[str]] = None):
     env: Env = Env()
@@ -22,62 +28,128 @@ def main(args: Optional[Iterable[str]] = None):
     env.info = info
     info("Starting up...")
 
-    # Dummy Global Path
-    from itertools import chain
-    env.path = np.fromiter(chain.from_iterable(
-        (2 * t, t ** 2 / 10) for t in np.arange(0.1, 10, 0.5)
-    ), dtype=np.float).reshape((-1, 2))
-
-    # Dummy obstacles
-    env.obstacles = np.fromiter(chain.from_iterable(
-        (3, y) for y in np.linspace(-1, -0.5, 5)
-    ), dtype=np.float).reshape((-1, 2))
-
-    # Dummy other vehicle
-    env.other_vehicle_states = [np.array([3, 5, - np.pi / 3, 0.3])]
-
     # visualize(env.m_pub, env.nh.get_clock(), 52, env.obstacles, color=ColorRGBA(g=1.0, a=1.0))
 
+    # TODO: Load the global path
+
     try:
-        run(env)
+        with rti.open_connector(config_name="SCADE_DS_Controller::Controller",
+                                url="resources/RtiSCADE_DS_Controller_ego1.xml") as connector:
+            # Readers
+            class Inputs:
+                sim_wait: Input = connector.get_input("simWaitSub::simWaitReader"),
+                vehicle_state: Input = connector.get_input("vehicleStateOutSub::vehicleStateOutReader")
+
+                def list(self) -> Iterable[Input]:
+                    return [self.sim_wait, self.vehicle_state]
+
+            inputs = Inputs()
+
+            # Writers
+            vehicle_correct = connector.getOutput("toVehicleModelCorrectivePub::toVehicleModelCorrectiveWriter")
+            vehicle_steer = connector.getOutput("toVehicleSteeringPub::toVehicleSteeringWriter")
+            sim_done = connector.getOutput("toSimDonePub::toSimDoneWriter")
+            sim_done.write()
+
+            while True:
+                for reader in inputs.list():
+                    reader.wait()
+                    reader.take()
+
+                # read values to env
+                state_in = inputs.vehicle_state.samples[-1]
+                env.state[0] = state_in["cdgPos_x"]
+                env.state[1] = state_in["cdgPos_y"]
+                env.state[1] = state_in["cdgSpeed_heading"]
+                env.state[2] = sqrt(state_in["cdgSpeed_x"] ** 2 + state_in["cdgSpeed_y"] ** 2)
+
+                # TODO: Load track Boundaries as Obstacles
+                env.obstacles
+
+                # TODO: Load dynamic vehicles
+                env.other_vehicle_states
+
+                # TODO: Load track boundaries
+                # env.track_boundry_poly
+
+                # Start stuff
+                if len(env.path) <= 10:
+                    break
+
+                trajectory = run(env)
+
+                # TODO: Run controller
+
+                # TODO: Publish values from controller
+                """
+                <struct name="CabToModelCorrective">
+                    <member name="AcceleratorAdditive" type="float64"/>
+                    <member name="AcceleratorMultiplicative" type="float64"/>
+                    <member name="BrakeAdditive" type="float64"/>
+                    <member name="BrakeMultiplicative" type="float64"/>
+                    <member name="ClutchAdditive" type="float64"/>
+                    <member name="ClutchMultiplicative" type="float64"/>
+                    <member name="GearboxAutoMode" type="int16"/>
+                    <member name="GearboxTakeOver" type="octet"/>
+                    <member name="IsRatioLimit" type="octet"/>
+                    <member name="MaxRatio" type="int16"/>
+                    <member name="MinRatio" type="int16"/>
+                    <member name="ParkingBrakeAdditive" type="float64"/>
+                    <member name="ParkingBrakeMultiplicative" type="float64"/>
+                    <member name="ShiftDown" type="octet"/>
+                    <member name="ShiftUp" type="octet"/>
+                    <member name="TimeOfUpdate" type="float64"/>
+                    <member name="WantedGear" type="int16"/>
+                </struct>
+                """
+                """
+                <struct name="CabToSteeringCorrective">
+                    <member name="AdditiveSteeringWheelAccel" type="float64"/>
+                    <member name="AdditiveSteeringWheelAngle" type="float64"/>
+                    <member name="AdditiveSteeringWheelSpeed" type="float64"/>
+                    <member name="AdditiveSteeringWheelTorque" type="float64"/>
+                    <member name="MultiplicativeSteeringWheelAccel" type="float64"/>
+                    <member name="MultiplicativeSteeringWheelAngle" type="float64"/>
+                    <member name="MultiplicativeSteeringWheelSpeed" type="float64"/>
+                    <member name="MultiplicativeSteeringWheelTorque" type="float64"/>
+                    <member name="TimeOfUpdate" type="float64"/>
+                </struct>
+                """
+
+                sim_done.write()
+
+
     except KeyboardInterrupt:
         info("Keyboard interrupt")
 
 
 def run(env: Env):
     info = env.info
-    while len(env.path) > 10:
-        # Remove passed points
-        # Note fails if robot and path have very different orientations, check for that
+    # Remove passed points
+    # Note fails if robot and path have very different orientations, check for that
 
-        update_global_path(env)
+    update_global_path(env)
 
-        # # Publish Global Path and Current Position
-        # visualize(env.m_pub, env.nh.get_clock(), 50, [env.state[:2]], scale=0.5,
-        #           color=ColorRGBA(r=1.0, b=1.0, a=1.0))
-        #
-        # visualize(env.m_pub, env.nh.get_clock(), 51, env.path)
+    # # Publish Global Path and Current Position
+    # visualize(env.m_pub, env.nh.get_clock(), 50, [env.state[:2]], scale=0.5,
+    #           color=ColorRGBA(r=1.0, b=1.0, a=1.0))
+    #
+    # visualize(env.m_pub, env.nh.get_clock(), 51, env.path)
 
-        # for i, state in enumerate(env.other_vehicle_states):
-        #     visualize(env.m_pub, env.nh.get_clock(), 52 + 1 + i, [state[:2]], scale=0.5,
-        #               color=ColorRGBA(r=1.0, g=1.0, a=1.0))
+    # for i, state in enumerate(env.other_vehicle_states):
+    #     visualize(env.m_pub, env.nh.get_clock(), 52 + 1 + i, [state[:2]], scale=0.5,
+    #               color=ColorRGBA(r=1.0, g=1.0, a=1.0))
 
-        paths = generate_paths(env, n=10, n_pts=20)
+    paths = generate_paths(env, n=10, n_pts=20)
 
-        best_trajectory, cost = score_paths(env, paths, max_path_len=20)
+    best_trajectory, cost = score_paths(env, paths, max_path_len=20)
 
-        if best_trajectory is not None:
-            info(f"Lowest {cost=:.2f}: {best_trajectory[1][:4]}")
-        else:
-            info("No trajectory found.")
+    if best_trajectory is not None:
+        info(f"Lowest {cost=:.2f}: {best_trajectory[1][:4]}")
+    else:
+        info("No trajectory found.")
 
-
-
-        # Dummy state update
-        #            [ x , y , theta, vel]
-        env.state += [0.5, 0.1, 0.05, 0.1]
-        for state in env.other_vehicle_states:
-            state[:2] += state[3] * np.array([np.cos(state[2]), np.sin(state[2])])
+    return best_trajectory
 
 
 def update_global_path(env: Env):
