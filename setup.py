5bc4b0009593ab1cf82dd8fcd9b4378740ca5cd3
Ashwin A Nayar <ashwin5059198@gmail.com>
2021-01-11 22:50:03 +0530

    remove ros

--- setup.py
+++ setup.py
@@ -7,26 +7,26 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-         ['resource/' + package_name]),  # Empty file to stop colcon complaints
+    # data_files=[
+    #     ('share/ament_index/resource_index/packages',
+    #      ['resource/' + package_name]),  # Empty file to stop colcon complaints
 
-        ('share/' + package_name, glob('launch/*.launch.py')),
+    #     ('share/' + package_name, glob('launch/*.launch.py')),
 
-        ('share/' + package_name, glob('rviz/*.rviz')),
+    #     ('share/' + package_name, glob('rviz/*.rviz')),
 
-        ('share/' + package_name, ['package.xml']),
-    ],
+    #     ('share/' + package_name, ['package.xml']),
+    # ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='suraj',
     maintainer_email='suraj.rathi00@gmail.com',
     description='TODO: Package description',
     license='Apache License 2.0',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            'main = iac_planner.main:main',
-        ],
-    },
+    # tests_require=['pytest'],
+    # entry_points={
+    #     'console_scripts': [
+    #         'main = iac_planner.main:main',
+    #     ],
+    # },
 )
